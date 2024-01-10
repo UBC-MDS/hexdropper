990f0154291e03c003512353118b1dc7c5e19703
joeywwwu <joeywwwu@outlook.com>
2024-01-09 22:15:22 -0800

    update rgb_to_hex function

---
+++ src/hexdropper/rgb_to_hex.py
@@ -0,0 +1,13 @@
+def rgb_to_hex(r, g, b):
+    """
+    Convert RGB color to Hexadecimal format.
+
+    Args:
+    r (int): Red component of the color, range 0-255.
+    g (int): Green component of the color, range 0-255.
+    b (int): Blue component of the color, range 0-255.
+
+    Returns:
+    str: Hexadecimal color codes in string.
+    """
+    return None
