de3006ecd9766f56825b2c90d7d93c2ec37ee8e1
joeywwwu <143630759+joeywwwu@users.noreply.github.com>
2024-01-11 20:44:39 -0800

    Update rgb_to_hex.py

--- src/hexdropper/rgb_to_hex.py
+++ src/hexdropper/rgb_to_hex.py
@@ -1,13 +1,31 @@
 def rgb_to_hex(r, g, b):
     """
-    Convert RGB color to Hexadecimal format.
+    Convert RGB color to hexadecimal format.
 
-    Args:
-    r (int): Red component of the color, range 0-255.
-    g (int): Green component of the color, range 0-255.
-    b (int): Blue component of the color, range 0-255.
+    Parameters
+    ----------
+    r : int
+        Red component of the color, range 0-255.
+    g : int
+        Green component of the color, range 0-255.
+    b : int
+        Blue component of the color, range 0-255.
 
-    Returns:
-    str: Hexadecimal color codes in string.
+    Returns
+    -------
+    str
+        The hexadecimal color code is a string. The format of the returned string is 'RRGGBB',
+        where RR is the hex representation of the red component, GG of the green, and BB of the blue.
+
+    Examples
+    --------
+    >>> rgb_to_hex(255, 0, 0)
+    'FF0000'  # Red color
+
+    >>> rgb_to_hex(0, 255, 0)
+    '00FF00'  # Green color
+
+    >>> rgb_to_hex(0, 0, 255)
+    '0000FF'  # Blue color
     """
     return None
