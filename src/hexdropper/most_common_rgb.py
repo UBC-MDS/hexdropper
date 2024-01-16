313407ebf55140104eee9eb1a2179081a2b0909a
JULIA EVERITT <juliaeveritt13@gmail.com>
2024-01-16 15:43:36 -0800

    "Lab work on read, most common functions"

--- src/hexdropper/most_common_rgb.py
+++ src/hexdropper/most_common_rgb.py
@@ -1,3 +1,5 @@
+import scipy.stats
+
 def most_common_rgb(input_array):
     """Determine the most common RGB values based on an image-derived array.
 
@@ -17,4 +19,11 @@ def most_common_rgb(input_array):
     --------
     >>> most_common_rgb(img_arr)
     """
-    
+
+    input_array = input_array/255 # normalize
+
+    width, height = input_array.shape[0], input_array.shape[1]
+
+    for i in range(width):
+        for j in range(height):
+            print(im[i][j]) # access individual pixels
